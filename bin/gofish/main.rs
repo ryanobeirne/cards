a6024b37409293710785171100ae435bd8dadb7b
Ryan O'Beirne <robeirne@spoonflower.com>
2019-10-11 17:15:05 -0400

    Start implementing go_fish

--- bin/gofish/main.rs
+++ bin/gofish/main.rs
@@ -45,10 +45,12 @@ fn play_game(n_players: usize) -> Result<Player> {
 
     let mut player_index = PlayerIndex::new(game.players.len());
     while !game.has_empty_hand() {
-        game.turn(&player_index)?;
         dbg!(&game);
+        game.turn(&player_index)?;
         player_index.increment();
     }
+
+    dbg!(&game);
 
     match game.players.into_iter().max_by(|a,b| a.paired.len().cmp(&b.paired.len())) {
         Some(player) => Ok(player),
@@ -90,9 +92,9 @@ impl Player {
         Ok(pairs.len())
     }
 
-    //fn has_value<V: Into<Value> + Copy>(&self, value: V) -> bool {
-        //self.hand.cards().any(|c| c.value == value.into())
-    //}
+    fn has_value<V: Into<Value> + Copy>(&self, value: V) -> bool {
+        self.hand.cards().any(|c| c.value == value.into())
+    }
 
     fn match_cards_from_value<V: Into<Value> + Copy>(&self, value: V) -> MatchIndex<Card> {
         self.hand.cards().enumerate()
@@ -176,27 +178,68 @@ impl FishGame {
                 let card = self.players[index.next].give(*i)?;
                 self.players[index.current].take(card);
             },
-            None => println!("Go fish!"),
+            None => {
+                let card = self.go_fish();
+                self.players[index.current].take(card);
+            },
         }
 
         self.players[index.current].discard_pairs()?;
 
         Ok(())
     }
+
+    fn go_fish(&mut self) -> Card {
+        let index = ask_user_index(&mut stdout(), self.river.len());
+        match self.river.give(index) {
+            Ok(card) => card,
+            Err(_) => {
+                eprintln!("Invalid selection!");
+                self.go_fish()
+            }
+        }
+    }
 }
 
-fn user_ask_value<W: Write>(writer: &mut W, player: &Player, next: &Player) -> Value {
-    write!(writer, "{}: Ask {} for a card value: ", player.name, next.name).expect("write");
-    writer.flush().expect("write");
+fn ask_user_index<W: Write>(w: &mut W, limit: usize) -> usize {
+    write!(w, "Go fish! [0-{}]: ", limit - 1).expect("write");
+    w.flush().expect("write");
+
+    let mut input = String::new();
+    stdin().read_line(&mut input).expect("stdin");
+
+    match input.trim().parse::<usize>() {
+        Ok(u) => {
+            if u < limit {
+                u
+            } else {
+                eprintln!("Index out of bounds!");
+                ask_user_index(w, limit)
+            }
+        },
+        Err(_) => ask_user_index(w, limit),
+    }
+} 
+
+fn user_ask_value<W: Write>(w: &mut W, player: &Player, next: &Player) -> Value {
+    write!(w, "{}: Ask {} for a card value: ", player.name, next.name).expect("write");
+    w.flush().expect("write");
 
     let mut input = String::new();
     stdin().read_line(&mut input).expect("stdin");
         
     match Value::try_from(input.trim()) {
-        Ok(v) => v,
+        Ok(v) => {
+            if player.has_value(v) {
+                v
+            } else {
+                writeln!(w, "You don't have that card!").expect("write");
+                user_ask_value(w, player, next)
+            }
+        },
         Err(_) => {
-            eprintln!("Invalid card value!");
-            return user_ask_value(writer, player, next);
+            writeln!(w, "Invalid card value!").expect("write");
+            user_ask_value(w, player, next)
         },
     }
 
